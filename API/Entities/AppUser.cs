7f9d16b51fbfe43b2ef84429cbee5717deefca98
dani.pernando70 <dani.pernando70@gmail.com>
2022-01-03 13:52:11 +0700

    first commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Umur { get; set; }
+        public string JenisKelamin { get; set; }
+    }
+}
